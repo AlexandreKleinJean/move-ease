93bfe2329eaaff0de60e97bf63f3eaa771c5dd9a
Alexandre Klein <alexandreklein@AirdeAlexandre.lan>
2024-08-18 18:15:21 +0200

    WireMock test in progress

---
+++ src/test/java/org/TvSerieResourceWiremockTest.java
@@ -0,0 +1,28 @@
+package org;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@QuarkusTest
+@QuarkusTestResource(TvSerieWiremockProxy.class)
+public class TvSerieResourceWiremockTest {
+
+  @Test
+  void getWithWireMock() {
+    given()
+        .when()
+        .param("title", "myTvSerie")
+        .get("/tvseries")
+        .then()
+        .body("id", equalTo(1))
+        .body("name", equalTo("myTvSerie"))
+        .body("url", equalTo("https://www.tvmaze.com/shows/2993/my-tv-series"))
+        .body("language", equalTo("English"))
+        .body("officialSite", equalTo("https://www.netflix.com/title/80057281"))
+        .statusCode(jakarta.ws.rs.core.Response.Status.OK.getStatusCode());
+  }
+}
