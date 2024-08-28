39c5694635ee0e4facb1bd5d6d5a6856e3b1133e
Alexandre Klein <alexandreklein@airdealexandre.home>
2024-08-28 18:24:48 +0200

    metrics + docker ok

--- src/main/java/org/gs/MicroProfileRestClient/TvSerieResource.java
+++ src/main/java/org/gs/MicroProfileRestClient/TvSerieResource.java
@@ -3,6 +3,10 @@ package org.gs.MicroProfileRestClient;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Metered;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import jakarta.ws.rs.GET;
@@ -21,6 +25,9 @@ public class TvSerieResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Counted(name= "timeFetchTvSeries", description = "Count of the fetch")
+    @Timed(name= "fetchDuration", description = "Duration of the fetch", unit = MetricUnits.MILLISECONDS)
+    @Metered(name= "fetchRate", description = "Rate of the fetch")
     public Response get(){
         TvSerie tvSerie = proxy.get("game of thrones");
         tvSeries.add(tvSerie);
